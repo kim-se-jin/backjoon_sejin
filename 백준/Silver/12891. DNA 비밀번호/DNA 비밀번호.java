a1d14adabf6d4620ac12462190fe2d3a9ee58322
kim-se-jin <67494004+kim-se-jin@users.noreply.github.com>
2023-03-07 13:54:58 +0900

    [Silver II] Title: DNA 비밀번호, Time: 292 ms, Memory: 21888 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -4,9 +4,9 @@ import java.util.*;
 public class Main{
 
 	static int S, P ;
-	static String DNAstr;
-	static int[] DNAnums ;
-
+	static char[] DNAstr;
+	static int[] DNAnums = new int[20] ; // 0 ="A", 1="C", 6="G", 19="T"
+	static int minA, minC, minG , minT ;
 	public static int stoi(String str){
 		return Integer.parseInt(str);
 	}
@@ -17,11 +17,15 @@ public class Main{
 
 		S = stoi(st.nextToken()); //DNA문자열 길이
 		P = stoi(st.nextToken()); //부분문자열 길이
-		DNAstr = br.readLine();
+		DNAstr = br.readLine().toCharArray(); // 문자열 입력
 
-		DNAnums = new int[4]; // A C G T
+		// 사용가능한 A,C,G,T 개수 입력받기
 		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<4;i++) DNAnums[i] = stoi(st.nextToken());
+		minA = stoi(st.nextToken());
+		minC = stoi(st.nextToken());
+		minG = stoi(st.nextToken());
+		minT = stoi(st.nextToken());
+
 
 		System.out.println(Sliding());
     }
@@ -30,39 +34,31 @@ public class Main{
 		int count = 0 ;
 		int start = 0 ; // 시작지점
 		int end = P; //끝지점
-		HashMap<Integer,Integer> hm = new HashMap<>();
 
 		for(int i=0;i<P;i++){ //P 길이만큼
-			int nowStr = DNAstr.charAt(i)-'A'; // 현재 문자
-			if(hm.containsKey(nowStr)) hm.put(nowStr, hm.get(nowStr)+1); // 이미 들어있는 문자라면 +1
-			else hm.put(nowStr, 1); // 없으면 1
+			int nowStr = DNAstr[i]-'A'; // 현재 문자
+			DNAnums[nowStr] ++ ;
 		}
 
-		if(CheckStr(hm)) count ++ ;
+		if(CheckStr()) count ++ ;
 		// System.out.println(hm.toString());
 
 		for(int i=P;i<S;i++){
-			int befStr = DNAstr.charAt(i-P)-'A'; // 이전 문자
-			hm.put(befStr, hm.get(befStr) -1);
+			int befStr = DNAstr[i-P]-'A'; // 이전 문자
+			DNAnums[befStr] -- ;
 
-			int nowStr = DNAstr.charAt(i)-'A'; // 현재 문자
-			if(hm.containsKey(nowStr)) hm.put(nowStr, hm.get(nowStr)+1); // 이미 들어있는 문자라면 +1
-			else hm.put(nowStr, 1); // 없으면 1
+			int nowStr = DNAstr[i]-'A'; // 현재 문자
+			DNAnums[nowStr] ++ ;
 
-			if(CheckStr(hm)) count ++ ;
-			// System.out.println(hm.toString());
-
+			if(CheckStr()) count ++ ;
 		}
 
 		return count ;
 	}
 
-	public static boolean CheckStr(HashMap hm){
+	public static boolean CheckStr(){
 		// DNAnums A C G T
-		if((int)hm.getOrDefault(0,0) < DNAnums[0]) return false ;
-		if((int)hm.getOrDefault(2,0) < DNAnums[1]) return false;
-		if((int)hm.getOrDefault(6,0) < DNAnums[2] ) return false;
-		if((int)hm.getOrDefault(19,0) < DNAnums[3]) return false;
-		return true ;
+		if(DNAnums[0] >= minA && DNAnums[2] >= minC && DNAnums[6]>=minG && DNAnums[19]>=minT) return true ;
+		else return false ;
 	}
 }
