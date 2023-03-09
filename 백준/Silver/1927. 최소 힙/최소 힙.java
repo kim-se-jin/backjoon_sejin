6f51c9b51d9200e8581c41ee86365864b1920430
kim-se-jin <67494004+kim-se-jin@users.noreply.github.com>
2023-03-10 08:43:13 +0900

    [Silver II] Title: 최소 힙, Time: 368 ms, Memory: 26988 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+	static int N;
+
+	public static int stoi(String str){
+		return Integer.parseInt(str);
+	}
+
+
+    public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = stoi(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer> pq = new PriorityQueue<>(((o1, o2) ->o1 - o2));
+
+		for(int i=0;i<N;i++){
+			int nowNum = stoi(br.readLine());
+			if(nowNum == 0 ){
+				if(pq.isEmpty()){
+					sb.append(0+"\n");
+				}else{
+					sb.append(pq.poll()+"\n");
+				}
+			}
+			else pq.add(nowNum);
+		}
+
+		System.out.println(sb.toString());
+
+	}
+}
