9aedf4e92f64465fd5621c9fca0c73fe1781e7d7
kim-se-jin <67494004+kim-se-jin@users.noreply.github.com>
2023-03-10 19:14:45 +0900

    [Silver I] Title: 회전 초밥, Time: 196 ms, Memory: 18880 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,54 +1,64 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
+
+
 public class Main {
- 
+
+    static int N,D,K,C;
+    static int[] arr ;
+	static int[] dishes ;
+
+    public static int stoi(String str){
+        return Integer.parseInt(str);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        int max=0;
-        
-        st = new StringTokenizer(br.readLine());
- 
-        int N = Integer.parseInt(st.nextToken()); //접시수
-        int d = Integer.parseInt(st.nextToken()); //초밥 수
-        int k = Integer.parseInt(st.nextToken()); //연속해서 먹는 접시수
-        int c = Integer.parseInt(st.nextToken()); //쿠폰번호
-        
-        int [] sushi = new int[N];
-        for(int i=0;i<N;i++) {
-            sushi[i] = Integer.parseInt(br.readLine());
-        }
-        
-        int [] check = new int[d+1];
-        int count=0;
-        
-        for(int i=0;i<k;i++) {
-            if(check[sushi[i]]==0) count++;
-            check[sushi[i]]++;
-        }
-        
-        max = count;
-        int start=1, end=k;
-        while(true) {
-            
-            if(check[sushi[start-1]]==1) count--;
-            check[sushi[start-1]]--;
-            
-            if(check[sushi[end]]==0) count++;
-            check[sushi[end]]++;
-            
-            if(check[c]==0) max = Math.max(max, count+1);
-            else max = Math.max(max, count);
-            
-            start++; end++;
-            if(end==N) end=0;
-            if(start==N) break;
-        }
-        System.out.println(max);
-            
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken()); // 접시의 수
+        D = stoi(st.nextToken()); // 초밥의 가짓수
+        K = stoi(st.nextToken()); // 연속해서 먹는 접시의 수
+        C = stoi(st.nextToken()); // 쿠폰 번호
+
+        arr = new int[N];
+		dishes = new int[D+1];
+        for(int i=0;i<N;i++) arr[i] = stoi(br.readLine());
+
+		Sliding();
+
     }
+
+	public static void Sliding(){
+		int count = 0 ;
+
+		//FirstEat
+		for(int i=0;i<K;i++){
+			if(dishes[arr[i]] == 0) count ++ ;
+				
+			dishes[arr[i]] ++ ; 
+		}
+		int MaxEat = dishes[C]==0?count+1:count;
+
+		for(int i=0;i<N;i++){
+			int removeIdx = (i)%N;
+			int addIdx = (i+K)%N;
+			
+			// 해당 접시 제거
+			if(dishes[arr[removeIdx]] == 1 ) count -- ; // 1개라면 카운트 1 감소, 해시맵 제거
+			dishes[arr[removeIdx]] -- ;
+			
+			// 추가
+			if(dishes[arr[addIdx]] == 0 ) count ++ ; // 현재 먹는 접시 추가
+			dishes[arr[addIdx]] ++ ;
+
+			// 쿠폰 먹었는 지 확인
+			MaxEat = Math.max(MaxEat, dishes[C]==0?count+1:count);
+		}
+
+		System.out.println(MaxEat);
+	}
+
+
 }
