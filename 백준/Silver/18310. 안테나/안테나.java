7804bb3e7e3e8f4f875f8019a3592c8d5c5f0bf2
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-03-13 21:39:08 +0900

    [Silver III] Title: 안테나, Time: 660 ms, Memory: 41584 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+	static int N;
+	static ArrayList<Integer> arr ;
+
+	// 모든 
+	public static int stoi(String str){
+		return Integer.parseInt(str);
+	}
+
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = stoi(br.readLine());
+		arr = new ArrayList<>();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++){
+			int now = stoi(st.nextToken());
+			arr.add(now); // 집 추가
+		}
+
+		Collections.sort(arr);
+		if(arr.size()%2 == 0) System.out.println(arr.get(arr.size()/2-1));
+		else System.out.println(arr.get(arr.size()/2));
+
+    }
+}
