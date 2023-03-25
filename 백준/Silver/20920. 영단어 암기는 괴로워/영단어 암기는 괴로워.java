476fb73bc3925556e2983144f06184abbb4720a7
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-03-25 22:10:02 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 740 ms, Memory: 43324 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -14,15 +14,11 @@ class voca implements Comparable<voca>{
 		// System.out.println(o.str + " , " + o.count);
 		if(this.count == o.count){
 
-			if(o.str.length() == this.str.length()){ // 우선순위 3. 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다
-				for(int i=0;i<o.str.length();i++){
-					if(o.str.charAt(i) != this.str.charAt(i)){
-						return this.str.charAt(i)-o.str.charAt(i);
-					}
-				}
-				return o.str.charAt(0)-this.str.charAt(0);
-			}else return o.str.length()-this.str.length(); // 우선순위 2. 해당 단어의 길이가 길수록 앞에 배치한다.
+			if(o.str.length() == this.str.length()) return this.str.compareTo(o.str); // 우선순위 3. 알파벳 사전 순으로 앞에 있는 단어일수록 앞에 배치한다
+			else return o.str.length()-this.str.length(); // 우선순위 2. 해당 단어의 길이가 길수록 앞에 배치한다.
+		
 		}else return o.count - this.count; // 우선순위 1. 자주 나오는 단어일수록 앞에 배치한다.
+		
 	}
 }
 
@@ -30,7 +26,7 @@ public class Main {
 
 	static int N,M;
 	static String str ;
-	static ArrayList<voca> arr ;
+	static PriorityQueue<voca> pq ;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
@@ -38,29 +34,25 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		arr = new ArrayList<>();
+		pq = new PriorityQueue<>();
 		HashMap<String, Integer> hm = new HashMap<>(); // 문자와 인덱스 저장
-		int idx = 0 ;
 
 		for(int i=0;i<N;i++){
 			str = br.readLine();
 			if(str.length() < M ) continue ; // M이상인 단어들만 외운다
-			if(hm.containsKey(str)){
-				// voca nowVoca = arr.get(hm.get(str));
-				// arr.remove(arr.get(hm.get(str)));
-				// arr.add(new voca(str, nowVoca.count+1));
-				voca now = arr.get(hm.get(str));
-				now.count ++ ;
-			}else{
-				hm.put(str,idx);
-				arr.add(new voca(str, 1));
-				idx++;
-			}
+			hm.put(str, hm.getOrDefault(str, 0)+1);
 		}
 
-		Collections.sort(arr);
+		for(String str : hm.keySet()){
+			pq.offer(new voca(str, hm.get(str)));
+		}
+
 		StringBuilder sb = new StringBuilder();
-		for(voca nVoca:arr) sb.append(nVoca.str+"\n");
+		while(!pq.isEmpty()){
+			voca nowVoca = pq.poll();
+			sb.append(nowVoca.str+"\n");
+		}
+
 		System.out.print(sb);
 
 	}
