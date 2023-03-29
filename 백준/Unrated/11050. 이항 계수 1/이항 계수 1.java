ed8328051a9d6cec3c4ed02b7cb58fb95a5e3446
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-03-30 00:57:18 +0900

    [Unrated] Title: 이항 계수 1, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Unrated/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        long top = factorial(n); //n!
+        long bottom = factorial(k) * factorial(n-k); // n!*(n-k)!
+        System.out.print(top / bottom);
+    }
+
+    // n!를 계산하는 함수
+    static long factorial(int n) {
+        long ret = 1;
+        while (n > 1) {
+            ret *= n;
+            n--;
+        }
+        return ret;
+    }
+}
+ 
