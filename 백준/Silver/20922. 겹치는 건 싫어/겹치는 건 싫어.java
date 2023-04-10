a5cab1b11fe9e7b498d4b1b6cccfb39fd24b6a5d
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-04-10 21:25:22 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 596 ms, Memory: 61980 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+
+    static int N,M,arr[];
+    static HashMap<Integer, Integer> hm;
+
+    public static int stoi(String str){
+        return Integer.parseInt(str);
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = stoi(st.nextToken());
+        M = stoi(st.nextToken());
+        arr = new int[N];
+        hm = new HashMap<>();
+        
+        String[] inputs = br.readLine().split(" ");
+        for(int i=0;i<N;i++) arr[i] = stoi(inputs[i]);
+        System.out.println(twoPointer());
+
+    }
+
+    public static int twoPointer(){
+        int start = 0 ;
+        int end = 0 ;
+        int ans = 0 ;
+
+        while(end<N && start<N){
+            // System.out.println("s : " + start + " ,e: " + end + ",hm: " + hm.toString());
+            while(end<N && hm.getOrDefault(arr[end], 0)<M){
+                // System.out.println("IN WHILE !! s : " + start + " ,e: " + end + ",hm: " + hm.toString());
+                // System.out.println(hm.getOrDefault(arr[end], 0));
+                hm.put(arr[end], hm.getOrDefault(arr[end], 0)+1);
+                end ++ ; 
+            }
+            ans = Math.max(ans, end-start);
+            hm.put(arr[start], hm.get(arr[start])-1);
+            start ++ ;
+            // hm.put(arr[start],hm.getOrDefault(arr[start], 0)+1);
+        }
+
+        if(ans == 0) ans = N ;
+        return ans;
+    }
+}
