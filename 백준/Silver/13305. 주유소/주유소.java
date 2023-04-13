6a48285fb9cd73d1936a0d2410f7ed67026db054
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-04-13 14:18:54 +0900

    [Silver III] Title: 주유소, Time: 236 ms, Memory: 26928 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -2,7 +2,8 @@ import java.io.*;
 import java.util.*;
  
 public class Main {
-	static int N,X,road[],cost[],dp[][];
+	static int N,X,road[],cost[];
+	static long dp[][];
  
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,16 +21,16 @@ public class Main {
 			cost[i] = Integer.parseInt(st.nextToken());
 		}
 
-		dp = new int[N][N];
+		dp = new long[N][N];
 		for(int i=0;i<N;i++){
 			for(int j=0;j<N;j++){
 				if(j==0) dp[i][j] = 0 ;
-				else dp[i][j] = Integer.MAX_VALUE;
+				else dp[i][j] = Long.MAX_VALUE;
 			}
 		}
 		for(int i=1;i<N;i++){
 			for(int j=i;j<N;j++){
-				dp[i][j] = Math.min(dp[i][j-1], dp[i-1][j-1])+ cost[i-1]*road[j-1];
+				dp[i][j] = Math.min(dp[i][j-1], dp[i-1][j-1])+ (long)cost[i-1]*road[j-1];
 				dp[i][j] = Math.min(dp[i][j], dp[i-1][j]);
 			}
 		}
