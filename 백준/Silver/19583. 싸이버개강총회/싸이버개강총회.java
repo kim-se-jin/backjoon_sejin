affdb06ee7a7dc46049d4848ad97c1394b905e57
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-04-19 23:18:35 +0900

    [Silver II] Title: 싸이버개강총회, Time: 588 ms, Memory: 54284 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 
 public class Main {
-	static int startTime, endTime1, endTime2 ;
 	static boolean[][] arr ;
+	static String[] times ;
 	static HashSet<String> hm ;
 
 	public static int stoi(String str){
@@ -12,10 +12,7 @@ public class Main {
 	}
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		startTime = calTime(st.nextToken().split(":"));
-		endTime1 = calTime(st.nextToken().split(":"));
-		endTime2 = calTime(st.nextToken().split(":"));
+		times = br.readLine().split(" ");
 		hm = new HashSet<>();
 		int cnt = 0 ;
 
@@ -23,36 +20,17 @@ public class Main {
 			String now = br.readLine();
 			if(now == null) break;
 			String[] inputs = now.split(" ");
-			int nowTime = calTime(inputs[0].split(":"));
 			if(hm.contains(inputs[1])){ // 이름이 있다면
 				// 나가기
-				if(checkAtrEnd(nowTime)){
+				if(inputs[0].compareTo(times[1]) >= 0 && inputs[0].compareTo(times[2]) <= 0){
 					hm.remove(inputs[1]);
 					cnt ++ ;
 				}
-
 			}else{ //없다면
 				// 개총 시간 이전 확인 -> 출석 인정
-				if(checkBefStart(nowTime)) hm.add(inputs[1]);
-
+				if(inputs[0].compareTo(times[0]) <= 0) hm.add(inputs[1]);
 			}
 		}
 		System.out.print(cnt);
-
     }
-
-	public static int calTime(String[] time){
-		return stoi(time[0])*60 + stoi(time[1]);
-	}
-
-	public static boolean checkBefStart(int nowTime){
-		if(nowTime <= startTime) return true ;
-		else return false ;
-	}
-
-	public static boolean checkAtrEnd(int nowTime){
-		if(nowTime == endTime1 || nowTime == endTime2) return true ;
-		if(nowTime >= endTime1 && nowTime <= endTime2) return true ;  
-		else return false ;
-	}
 }
