ab9f77600598e72d1fbcce992c691cabf436a3cb
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-04-26 17:12:52 +0900

    [Silver III] Title: 카드 문자열, Time: 1420 ms, Memory: 40256 KB -BaekjoonHub

---
+++ 백준/Silver/13417. 카드 문자열/카드 문자열.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	static int N;
+	static String inputs[];
+	
+	public static int stoi(String str){
+		return Integer.parseInt(str);
+	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		int tc = stoi(br.readLine());
+		while(tc-->0){
+			N = stoi(br.readLine());
+			inputs = br.readLine().split(" ");
+
+			Deque<String> dq = new ArrayDeque<>();
+			dq.add(inputs[0]);
+
+			for(int i=1;i<N;i++){
+				if(inputs[i].compareTo(dq.getFirst())>0){
+					dq.offerLast(inputs[i]);
+				}else{
+					dq.offerFirst(inputs[i]);
+				}
+			}
+
+			for(int i=0;i<N;i++) System.out.print(dq.poll());
+			System.out.println();
+		}
+    }
+}
