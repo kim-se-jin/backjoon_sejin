060f21421e2b97195423b5c9575f84a9fb6f4ad4
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-04-27 20:41:11 +0900

    [Gold V] Title: 신기한 소수, Time: 140 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Main {
 	static int N;
+	static StringBuilder sb;
 	
 	public static int stoi(String str){
 		return Integer.parseInt(str);
@@ -9,35 +10,28 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = stoi(br.readLine());
+		sb = new StringBuilder();
 		
-		// 1 일 떄 : 2,3,5,7
-		// 2 일 때 : 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61
-		prime("2",1);
-		prime("3",1);
-		prime("5",1);
-		prime("7",1);
-
-
+		prime(0,0);
+		System.out.print(sb);
     }
 
-	public static void prime(String str, int depth){
+	public static void prime(int now, int depth){
 
 		if(depth == N){
-			System.out.println(str);
+			sb.append(now+"\n");
 			return ;
 		}
-		for(int i=1;i<=9;i++){
-			String nowNum = str + String.valueOf(i);
-			// System.out.println(nowNum);
 
-			if(check(Integer.valueOf(nowNum))){
-				prime(nowNum,depth+1);
-			}
+		for(int i=1;i<=9;i++){
+			int nowNum = now*10 + i;
+			if(check(nowNum)) prime(nowNum,depth+1);
 		}
 	} 
 
 	public static boolean check(int nowNum){
-		for(int i=2;i<nowNum;i++){
+		if(nowNum < 2 ) return false ;
+		for(int i=2;i<=Math.sqrt(nowNum);i++){
 			if(nowNum%i == 0) return false ;
 		}
 		return true ;
