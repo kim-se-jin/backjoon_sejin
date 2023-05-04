8e297cd48c58376c658d43b971ad89f42e2af702
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-05-04 17:59:18 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 212 ms, Memory: 15712 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -1,21 +1,9 @@
 import java.io.*;
 import java.util.*;
 
-class egg{
-	int power, w;
-	boolean gone ;
-	egg(int power, int w, boolean gone){
-		this.power = power;
-		this.w = w ;
-		this.gone = gone;
-	}
-}
-
 public class Main {
 
-	static int N, ans;
-	static egg[] arr;
-	static boolean[] live;
+	static int N, ans,power[], w[];
 
 	public static int stoi(String str){
 		return Integer.parseInt(str);
@@ -25,60 +13,53 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = stoi(br.readLine());
 		StringTokenizer st ;
+		ans = Integer.MIN_VALUE;
 
-		arr = new egg[N];
+		power = new int[N];
+		w = new int[N];
 
 		for(int i=0;i<N;i++){
 			st = new StringTokenizer(br.readLine());
-			arr[i] = new egg(stoi(st.nextToken()), stoi(st.nextToken()),false);
+			power[i] = stoi(st.nextToken());
+			w[i] = stoi(st.nextToken());
 		}
-		conflict(0, arr, 0);
+		conflict(0, 0);
 		System.out.print(ans);
     }
 
-	public static void conflict(int idx, egg[] arr, int count){
-		if(idx == N) return ;
-		if(arr[idx].gone){
-			conflict(idx+1, arr, count);
+	public static void conflict(int idx, int count){
+		if(idx == N){
+			ans = Math.max(ans, count);
+			return ;
+		}
+
+		if(power[idx] <= 0 || count == N-1){ // 이미 깨진 계란이라면 
+			conflict(idx+1, count); // 다음꺼
 			return;
 		}
 
-		for(int i=0;i<arr.length;i++){ // 가장 왼쪽꺼 드니까 1 부터 하나씩 깨보기
+		int nowCnt = count ;
+		for(int i=0;i<N;i++){
 			if(idx == i ) continue ;
-
-			// System.out.println("for start : " + i );
-			// 깊은 복사
-			egg[] newArr = copy(arr);
-			int cnt = 0 ;
-
-			egg nowEgg = newArr[idx];
-			egg nxtEgg = newArr[i];
-
-			if(newArr[i].gone) continue ; // 이미 깨진 계란인 경우 
+			if(power[i] <= 0 ) continue ; // 이미 깨진 계란인 경우 
 
 			// 각 계란의 내구도는 상대 계란의 무게만큼 깎임
-			newArr[idx].power -= nxtEgg.w ;
-			newArr[i].power -= nowEgg.w ;
-			if(newArr[idx].power <= 0 ){
-				cnt ++ ;
-				newArr[idx].gone = true ;
-			}
-			if(newArr[i].power <= 0 ){
-				cnt ++ ;
-				newArr[i].gone = true ;
-			}
-			// printegg(newArr);
-			
-			ans = Math.max(ans, count + cnt);
-			conflict(idx+1, newArr, count + cnt );
+			hitEgg(idx, i);
+			if(power[idx] <= 0 ) count ++ ;
+			if(power[i] <= 0 ) count ++ ;
+			conflict(idx+1, count);
+			returnEgg(idx, i);
+			count = nowCnt;
 		}
 	}
 
-	public static egg[] copy(egg[] arr){
-		egg[] newEgg = new egg[N];
-		for(int i=0;i<N;i++){
-			newEgg[i] = new egg(arr[i].power, arr[i].w,arr[i].gone);
-		}
-		return newEgg ;
+	public static void hitEgg(int x, int y){
+		power[x] -= w[y] ;
+		power[y] -= w[x] ;
+	}
+
+	public static void returnEgg(int x, int y){
+		power[x] += w[y] ;
+		power[y] += w[x] ;
 	}
 }
