fe1968eca7d18a49ca6d5a53792359799c6b820f
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-05-11 20:15:49 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 int solution(int n)
 {
     int answer = 0;
-    while(n!=0){
-        answer += n%10;
-        n/=10;
-    }
+    string s = to_string(n);
+    for(int i=0;i<s.size();i++) answer += s[i]-'0';
     
     return answer;
 }
