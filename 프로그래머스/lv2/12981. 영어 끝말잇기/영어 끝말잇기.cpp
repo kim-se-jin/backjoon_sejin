9e039794ecaee495d6848c6e042d7d12494c1fb5
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-05-12 17:52:34 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(int n, vector<string> words) {
+    vector<int> answer;
+    map<string, int> word;
+    word[words[0]]++;
+    
+    for(int i=1;i<words.size();i++){
+        if(word[words[i]] || words[i].front() != words[i-1].back()){
+            return {(i%n)+1, (i/n)+1};
+        }
+        word[words[i]]++;
+    }
+
+    return {0,0};
+}
