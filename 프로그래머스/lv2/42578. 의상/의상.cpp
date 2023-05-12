361d43ddc8cf451cb1112d4dd33a57c05eece167
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-05-12 19:19:52 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.cpp
+++ 프로그래머스/lv2/42578. 의상/의상.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <string>
 #include <vector>
 #include <unordered_map>
 
@@ -6,34 +6,15 @@ using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
-    unordered_map<string, int> hash_map;
-
-    // 각 의상의 종류별로 개수를 센다
-    for (int i = 0; i < clothes.size(); i++) {
-        string category = clothes[i][1];  // 의상의 종류
-        hash_map[category]++;
+    unordered_map<string, int> map ;
+    for(int i=0;i<clothes.size();i++){
+        string cate = clothes[i][1];
+        map[cate] ++ ;
     }
-
-    // (의상의 종류별 개수 + 1)을 곱해준다
-    for (auto it = hash_map.begin(); it != hash_map.end(); it++) {
-        answer *= (it->second + 1);
+    
+    for(auto it=map.begin();it!=map.end();it++){
+        answer *= (it->second+1);
     }
-
-    // 아무것도 입지 않은 경우를 제외한다
-    answer--;
-
-    return answer;
-}
-
-int main() {
-    vector<vector<string>> clothes = {
-        {"yellow_hat", "headgear"},
-        {"blue_sunglasses", "eyewear"},
-        {"green_turban", "headgear"}
-    };
-
-    int result = solution(clothes);
-    cout << "Number of possible combinations: " << result << endl;
-
-    return 0;
+    
+    return answer-1;
 }
