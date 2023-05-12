a64d986a4d3c2e295f1f58fae186fc429ffaf6ad
김세진 <67494004+kim-se-jin@users.noreply.github.com>
2023-05-12 15:35:16 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.09 ms, Memory: 3.77 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+    int N = A.size();
+
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+    
+    for(int i=0;i<N;i++){
+        answer += (A[i] * B[N-i-1]);
+    }
+
+    return answer;
+}
